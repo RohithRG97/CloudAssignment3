a05d1b01b86679cd4b681e26e82b8a96aba876de
Rohith Gurram <rohith9998@gmail.com>
2023-03-06 21:32:38 -0600

    Commit 3

--- app.py
+++ app.py
@@ -84,18 +84,57 @@ def filteredqueryq11():
         rank2 = request.form.get("rk2")
         state = request.form.get("st")
         num = request.form.get("num")
+
+        timetaken =  []
         
         for i in range(int(num)):
+            start_time = time.time()
             sql = "SELECT city, state, rank, population FROM dbo.cities WHERE state = ? AND rank BETWEEN ? AND ?"
             values = (state, rank1, rank2)
 
             cursor = conn.cursor()
             cursor.execute(sql, values)
             results = cursor.fetchall()
-   
+            end_time = time.time()
+            total_time = end_time - start_time
+            timetaken.append(total_time)
+        
+        return render_template("filtered_queryq11.html", timetaken = timetaken, data = results)
+
+@app.route('/redisq12.html', methods =["GET", "POST"])
+def redisq12():
+    if request.method == "POST":
+        start_time = time.time()
+        for i in range(1000):
+            
+            # generate random values for the query
+            lat = random.uniform(-90, 90)
+            long = random.uniform(-180,180)
+            depth = random.uniform(1, 20)
+            
+            # build the query
+            # sql1 = "SELECT * FROM dbo.earthquake_data WHERE latitude = ? AND longitude = ? AND depth = ?"
+            # values = (lat, long, depth)
+
+            cursor = conn.cursor()
+            sql = "SELECT * FROM dbo.earthquake_data WHERE lat = ? and long = ?"
+            values = (lat, long)
+            my_string = json.dumps(values)
+            sql_key = my_string
+
+            if redis_client.exists(sql_key):
+                results = pickle.loads(redis_client.get(sql_key))
+            else:
+                cursor = conn.cursor()
+                cursor.execute(sql, values)
+                columns = [column[0] for column in cursor.description]
+                rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
+                redis_client.set(sql_key, pickle.dumps(rows), ex=3600)
+
         end_time = time.time()
         total_time = end_time - start_time
-        return render_template("filtered_queryq11.html", time=total_time, data = results)
+        return render_template("time_1000query_redis.html", time=total_time)
+
 
 @app.route('/time_1000query_redis.html', methods =["GET", "POST"])
 def redis():
@@ -130,6 +169,8 @@ def redis():
         end_time = time.time()
         total_time = end_time - start_time
         return render_template("time_1000query_redis.html", time=total_time)
+    
+
 
 
 if __name__ == "__main__":
